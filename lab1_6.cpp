2090ce3eb05fff733375386794954fd9d6e637f8
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-01 18:45:27 +0700

    Variable type changing & more codes addiction.

--- lab1_6.cpp
+++ lab1_6.cpp
@@ -1,12 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	unsigned int left, right, total, side;
+	unsigned int left, right, total;
+	char side;
 	/*
 		1 Cycle = left+right
 		total = total % Cycle
 	*/
 	scanf("%d %d %d", &left, &right, &total);
 	total = total%(left+right);
+	scanf("%c", &side);
+	if (side == 'L')
+	{
+		side = 'L';
+	}
+	else
+	{
+		side = 'R';
+	}
 	return 0;
 }
