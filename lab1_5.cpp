1b9170477f2c57ca1cc3a2601ffb772b3ff59f58
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-01 18:18:59 +0700

    Variable & more command addiction

--- lab1_5.cpp
+++ lab1_5.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	int n, max=0b10, get;
+	int n, max=-2147483647, get, pos;
 	scanf("%d", &n);
 	while (n > 0)
 	{
 		scanf("%d", &get);
-		
+		if (get > max)
 		n--;
-		
 	}
 	return 0;
 }
