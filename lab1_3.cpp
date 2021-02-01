13896727378fd6fd9334df1076fe44abcb295f6f
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-01 20:04:48 +0700

    Raise a white flag

--- lab1_3.cpp
+++ lab1_3.cpp
@@ -3,30 +3,6 @@ int main()
 {
 	unsigned long long get;
 	scanf("%ld", &get);
-	while (get >= 1291467969000000)
-	{
-		get -= 3913539300000;
-	}
-	while (get >= 3913539300000)
-	{
-		get -= 11859210000;
-	}
-	while (get >= 11859210000)
-	{
-		get -= 35937000;
-	}
-	while (get >= 35937000)
-	{
-		get -= 1086900;
-	}
-	while (get >= 1086900)
-	{
-		get -= 330;
-	}
-	while (get >= 330)
-	{
-		get -= 33;
-	}
 	printf("%d %d", get%3, get%11);
 	return 0;
 }
